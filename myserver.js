db8c05a230be95327c71d5aca622d7c93f79eaea
asdfghjkl43m <59603278+asdfghjkl43m@users.noreply.github.com>
2020-02-27 16:12:13 +0700

    hhh

--- myserver.js
+++ myserver.js
@@ -2,14 +2,27 @@ var express=require('express');
 var app=express();
 
 app.set('view engine','jade');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4000;
 
 var publicDir = require('path').join(__dirname,'/public');
 app.use(express.static(publicDir));
+
+var bodyParser= require('body-Parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 //khi mo website mac dinh
 app.get("/",(req,res)=>{
     res.render('index')
 })
 
+app.get('/form',(req,res)=>{
+    res.render('form');
+})
+
+app.post('/saveForm',(req,res)=>{
+    //lay thong tin tu textbox ten la yourname
+    const yourName=req.body.yourName;
+    res.render('confirm',{name:yourName})
+
+})
 var server=app.listen(port,function() {});
