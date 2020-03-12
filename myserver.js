69371322a4102de59f9dc659fd5d23845877dbbb
asdfghjkl43m <59603278+asdfghjkl43m@users.noreply.github.com>
2020-03-12 11:35:44 +0700

    asdas
    asd

--- myserver.js
+++ myserver.js
@@ -2,7 +2,7 @@ var express=require('express');
 var app=express();
 
 app.set('view engine','jade');
-var port = process.env.PORT || 11000;
+var port = process.env.PORT || 12000;
 
 var publicDir = require('path').join(__dirname,'/public');
 app.use(express.static(publicDir));
@@ -26,11 +26,31 @@ app.get('/mugi',(req,res)=>{
 app.get('/violet',(req,res)=>{
     res.render('violet');
 })
-
+app.get('/violetbuy',(req,res)=>{
+    res.render('violetbuy');
+})
+app.get('/yuibuy',(req,res)=>{
+    res.render('yuibuy');
+})
+app.get('/mugibuy',(req,res)=>{
+    res.render('mugibuy');
+})
+app.get('/registerform',(req,res)=>{
+    res.render('registerform');
+})
+app.post('/yuiBack',(req,res)=>{
+    res.render('yui')
+})
+app.post('/violetback',(req,res)=>{
+    res.render('violet')
+})
 app.post('/saveForm',(req,res)=>{
-    //lay thong tin tu textbox ten la yourname
-    const yourName=req.body.yourName;
-    res.render('confirm',{name:yourName})
-
+    res.render('confirm')
+})
+app.post('/mugiBack',(req,res)=>{
+    res.render('mugi')
+})
+app.post('/logininstead',(req,res)=>{
+    res.render('form')
 })
 var server=app.listen(port,function() {});
